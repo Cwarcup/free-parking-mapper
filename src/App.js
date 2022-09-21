dfdced881361a7be9476cd429f0a55ae145e372c
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-21 14:24:55 -0700

    Initial setup of map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
-	return <Button variant="contained">Hello World</Button>;
+	const Map = ReactMapboxGl({
+		accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+	});
+
+	return (
+		<>
+			<Map
+				center={{ lon: -87.621, lat: 41.874 }}
+				style="mapbox://styles/curtiswarcup/cl8c2ly0s000u15mjwaive8g8"
+				containerStyle={{
+					height: "50vh",
+					width: "50vw",
+				}}
+			>
+				<Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+					<Feature coordinates={[41.874, -87.621]} />
+				</Layer>
+			</Map>
+			;
+		</>
+	);
 }
 
 export default App;
