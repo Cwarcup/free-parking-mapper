c9e5d057ba608644c394c5dacbbcc15cc8a3fb99
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-21 16:13:17 -0700

    Adding points

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import * as React from "react";
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+import Map, { Source, Layer } from "react-map-gl";
 
 function App() {
-	const Map = ReactMapboxGl({
-		accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
-	});
+	const long = -87.621;
+	const lat = 41.874;
+
+	const geojson = {
+		type: "FeatureCollection",
+		features: [
+			{
+				type: "Feature",
+				geometry: { type: "Point", coordinates: [long, lat] },
+			},
+		],
+	};
+
+	const layerStyle = {
+		id: "point",
+		type: "circle",
+		paint: {
+			"circle-radius": 10,
+			"circle-color": "#007cbf",
+		},
+	};
 
 	return (
 		<>
 			<Map
-				center={{ lon: -87.621, lat: 41.874 }}
-				style="mapbox://styles/curtiswarcup/cl8c2ly0s000u15mjwaive8g8"
-				containerStyle={{
-					height: "50vh",
-					width: "50vw",
+				mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+				initialViewState={{
+					longitude: -87.621,
+					latitude: 41.874,
+					zoom: 14,
 				}}
+				style={{ width: 600, height: 400 }}
+				mapStyle="mapbox://styles/curtiswarcup/cl8c2ly0s000u15mjwaive8g8"
 			>
-				<Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
-					<Feature coordinates={[41.874, -87.621]} />
-				</Layer>
+				<Source type="geojson" data={geojson}>
+					<Layer {...layerStyle} />
+				</Source>
 			</Map>
-			;
 		</>
 	);
 }
