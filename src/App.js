511c63c3c8ab91778f512fcf6df0a943fc67957c
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-22 10:47:46 -0700

    Start of components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import * as React from "react";
-import Map, { Source, Layer } from "react-map-gl";
+import MapMain from "./components/MapMain";
 
 function App() {
-	const long = -123.137414;
-	const lat = 49.163168;
-	const geojson = {
-		type: "FeatureCollection",
-		features: [
-			{
-				type: "Feature",
-				geometry: { type: "Point", coordinates: [long, lat] },
-			},
-		],
-	};
-
-	const layerStyle = {
-		id: "point",
-		type: "circle",
-		paint: {
-			"circle-radius": 10,
-			"circle-color": "#007cbf",
-		},
-	};
-
 	return (
 		<>
-			<Map
-				mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-				initialViewState={{
-					longitude: long,
-					latitude: lat,
-					zoom: 12,
-				}}
-				style={{ width: 600, height: 400 }}
-				mapStyle="mapbox://styles/curtiswarcup/cl8c8zo7h000314qno3pumnrz"
-			>
-				<Source type="geojson" data={geojson}>
-					<Layer {...layerStyle} />
-				</Source>
-			</Map>
+			<MapMain />
 		</>
 	);
 }
