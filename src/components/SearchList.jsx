3b39093cd378959f776d0f0d93a2b0a901fd89f1
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-04 13:53:21 -0700

    background change when search list selected

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -4,6 +4,7 @@ import {
 	SelectedItemContext,
 	SearchInfoContext,
 } from "../helpers/context";
+import selectedSVG from "../images/selectedItemSvg.svg";
 
 const SearchList = () => {
 	const { searchResults } = useContext(SearchContext);
@@ -12,6 +13,12 @@ const SearchList = () => {
 
 	const [resultLength, setResultLength] = useState(3);
 	const [list, setList] = useState([]);
+	const [selected, setSelected] = useState("hidden");
+
+	// change the background color of the selected list item when clicked
+	const handleSelect = (e) => {
+		setSelected(e.target.id);
+	};
 
 	useEffect(() => {
 		if (searchResults) {
@@ -19,8 +26,10 @@ const SearchList = () => {
 				return (
 					<li key={`search-result-${index}`}>
 						<button
-							className="flex flex-col items-center focus:bg-slate-600 hover:bg-slate-600 w-full space-x-2 rounded-md px-10 py-4 text-gray-300 focus:outline-none"
-							onClick={() => {
+							className={`flex flex-col items-center focus:bg-slate-600 hover:bg-slate-600 w-full space-x-2 rounded-md px-10 py-4 text-gray-300 focus:outline-none {${
+								selected === `search-result-${index}` ? "bg-slate-600" : ""
+							}}`}
+							onClick={(e) => {
 								setSelectedItem(result);
 								setSearchInfo({
 									...searchInfo,
@@ -29,6 +38,8 @@ const SearchList = () => {
 									address: result.properties.address,
 									category: result.properties.category,
 								});
+
+								handleSelect(e);
 							}}
 						>
 							<p className="text-gray-100 ">{result.text}</p>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,7 +4,6 @@ import { BiSearch } from "react-icons/bi";
 import SearchList from "./SearchList";
 
 const SideBar = () => {
-
 	return (
 		<>
 			<aside className="fixed z-50 md:relative">
@@ -45,89 +44,9 @@ const SideBar = () => {
 						<BiSearch className="absolute left-2 block h-5 w-5 text-gray-400" />
 						<SearchInput />
 					</div>
-					<SearchList />
-					<ul className="mt-8 space-y-3 md:mt-20">
-						<li className="relative">
-							<button className="focus:bg-slate-600 hover:bg-slate-600 flex w-full space-x-2 rounded-md px-10 py-4 text-gray-300 focus:outline-none">
-								<span>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										className="h-6 w-6"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										strokeWidth="2"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-										/>
-									</svg>
-								</span>
-								<span className="">Overview</span>
-							</button>
-						</li>
-						<li className="relative">
-							<button className="focus:bg-slate-600 hover:bg-slate-600 flex w-full space-x-2 rounded-md px-10 py-4 font-semibold focus:outline-none">
-								<span>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										className="h-6 w-6"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										strokeWidth="2"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"
-										/>
-									</svg>{" "}
-								</span>
-								<span className="">Meters</span>
-							</button>
-							<svg
-								className="text-slate-200 absolute -right-1 -top-1/2 z-10 hidden h-32 w-8 md:block"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="399.349 57.696 100.163 402.081"
-								width="1em"
-								height="4em"
-							>
-								<path
-									fill="currentColor"
-									d="M 499.289 57.696 C 499.289 171.989 399.349 196.304 399.349 257.333 C 399.349 322.485 499.512 354.485 499.512 458.767 C 499.512 483.155 499.289 57.696 499.289 57.696 Z"
-								/>
-							</svg>
-						</li>
-						<li className="relative">
-							<button className="focus:bg-slate-600 hover:bg-slate-600 flex w-full space-x-2 rounded-md px-10 py-4 text-gray-300 focus:outline-none">
-								<span>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										className="h-6 w-6"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										strokeWidth="2"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-										/>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-										/>
-									</svg>
-								</span>
-								<span className="">Settings</span>
-							</button>
-						</li>
-					</ul>
+					<div className="mt-5">
+						<SearchList />
+					</div>
 
 					<div className="my-6 mt-auto ml-10 flex cursor-pointer">
 						<div>
